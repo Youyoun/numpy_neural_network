228e1bf6ac3f4c46d95fb72ed5764ef5318bdd60
Younes Belkouchi <belkouchiyounes@gmail.com>
2018-06-03 18:46:30 +0200

    Feat: Added all tests to iris_test.py

--- iris_test.py
+++ iris_test.py
@@ -9,19 +9,35 @@ LEARNING_RATE = 0.04
 ACTIVATION = act.ReLU
 LOSS_FN = loss.CrossEntropyLoss
 LAYERS = (10,)
+EPOCHS = 1000
+ITER = 18
 
 DATASET = load_iris()
 INPUTS = DATASET.data
 OUTPUTS = DATASET.target
 X_train, X_test, Y_train, Y_test = train_test_split(INPUTS, OUTPUTS, test_size=0.2, random_state=42)
 
-a = nn.Net(hidden_layers=LAYERS, activation=ACTIVATION, loss_function=LOSS_FN, lr=LEARNING_RATE, descent="stochastic",
-           save_metrics=True, dropout=True)
+gr_descent = nn.Net(hidden_layers=LAYERS, activation=ACTIVATION, loss_function=LOSS_FN, lr=LEARNING_RATE,
+                    save_metrics=True)
+stgr_descent = nn.Net(hidden_layers=LAYERS, activation=ACTIVATION, loss_function=LOSS_FN, lr=LEARNING_RATE,
+                      descent="stochastic", save_metrics=True)
+gr_descent_dropout = nn.Net(hidden_layers=LAYERS, activation=ACTIVATION, loss_function=LOSS_FN, lr=LEARNING_RATE,
+                            save_metrics=True, dropout=True)
+stgr_descent_dropout = nn.Net(hidden_layers=LAYERS, activation=ACTIVATION, loss_function=LOSS_FN, lr=LEARNING_RATE,
+                              descent="stochastic",
+                              save_metrics=True, dropout=True)
 
-a.fit(X_train, Y_train, epochs=500)
-result = a.predict(X_test)
-print(Y_test)
-print(result)
+gr_descent.fit(X_train, Y_train, epochs=EPOCHS)
+stgr_descent.fit(X_train, Y_train, iter=ITER, epochs=EPOCHS)
+gr_descent_dropout.fit(X_train, Y_train, epochs=EPOCHS)
+stgr_descent_dropout.fit(X_train, Y_train, iter=ITER, epochs=EPOCHS)
+result = gr_descent.predict(X_test)
+result_st = stgr_descent.predict(X_test)
+result_drop = gr_descent_dropout.predict(X_test)
+result_st_drop = stgr_descent_dropout.predict(X_test)
+# print(Y_test)
+# print(result)
 print("Accuracy: {}".format(np.sum(result == Y_test) / len(Y_test)))
-
-# accuracy vs epoch, loss vs epoch, loss vs learning rate, loss vs iter
+print("Accuracy stochastic: {}".format(np.sum(result_st == Y_test) / len(Y_test)))
+print("Accuracy with dropout: {}".format(np.sum(result_drop == Y_test) / len(Y_test)))
+print("Accuracy stochastic with dropout: {}".format(np.sum(result_st_drop == Y_test) / len(Y_test)))
